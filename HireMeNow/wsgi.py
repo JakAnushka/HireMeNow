6938563903d7da54ff6efb53233c00d069c1b7af
Anushka <118365351+JakAnushka@users.noreply.github.com>
2025-09-01 19:37:14 +0530

    wsgi.py

--- HireMeNow/wsgi.py
+++ HireMeNow/wsgi.py
@@ -13,4 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HireMeNow.settings')
 
+
 application = get_wsgi_application()
+HireMeNow.wsgi:application
+
